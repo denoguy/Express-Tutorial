951061e96c740aa3a20d66999e22dfcf7e5b5dfd
Lokendra Dangi <pachar.com@gmail.com>
2020-05-30 14:04:11 +0545

    Rendering Static HTML with Public Directory

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
+const path = require('path')
 const express = require('express')
 
+
+const publicPath = path.join(__dirname, '../public')
 const app = express()
+
+
+app.use(express.static(publicPath));
+
+
 
 app.get('', (req, res) => {
     res.send('<h1>Hello Express</h1>')
