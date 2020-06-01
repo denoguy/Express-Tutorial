592977cc18c92259812f4daf0304a0c6f32cb9ae
Lokendra Dangi <pachar.com@gmail.com>
2020-06-01 22:54:08 +0545

    Customize View Directory and location

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 
 
 const publicPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates')
 const app = express()
 
 app.set('view engine', 'hbs')
-
+app.set('views', viewPath)
 app.use(express.static(publicPath));
 
 app.get('', (req, res) => {
